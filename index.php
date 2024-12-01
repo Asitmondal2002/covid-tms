477405ea75e8779db3a5e66cb94c81cad6aa5971
Asit Mondal <mondalasit0143@gmail.com>
2024-12-01 09:13:30 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid-19 Testing Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">COVID-19</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About Coronavirus </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Covid-19 Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Prevention</a>
+          </li>
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>COVID19-TMS</h1>
+      <p class="lead">COVID19 - Testing Management System</p>
+    </div>
+  </header> -->
+
+  <!-- Slide  -->
+
+  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="2000">
+      <img src="img/test_01.jpg" class="d-block w-100" height="620" alt="...">
+      <div class="carousel-caption d-none d-md-block text-center">
+       <h1>COVID19-TMS</h1>
+       <p class="lead">COVID19 - Testing Management System</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="img/test_02.jpg" class="d-block w-100" height="620" alt="...">
+      <div class="carousel-caption d-none d-md-block text-center">
+       <h1>COVID19-TMS</h1>
+       <p class="lead">COVID19 - Testing Management System</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="img/test_3.jpg" class="d-block w-100" height="620" alt="...">
+      <div class="carousel-caption d-none d-md-block text-center">
+       <h1>COVID19-TMS</h1>
+       <p class="lead">COVID19 - Testing Management System</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="img/test_4.jpg" class="d-block w-100" height="620" alt="...">
+      <div class="carousel-caption d-none d-md-block text-center">
+       <h1>COVID19-TMS</h1>
+       <p class="lead">COVID19 - Testing Management System</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+  <!-- Slide end -->
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+
+          <h2 class="text-center">About this page</h2>
+          <p class="lead">Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19, virus will experience mild to moderate, respiratory illness & recover without requiring special treatment. Older people and those with underlying medical problem like cardiovascular disease.</p>
+          <p class="lead"> COVID-19 website provides crucial information and updates on the pandemic. It offers real-time data on cases, testing, vaccinations, and safety guidelines. Visitors can access state-specific details, travel advisories, and vaccination registration links. The website also features resources like FAQs, myth-busters, and contact information for health authorities. Stay informed and follow recommended precautions to protect yourself and others. Visit the website regularly for the latest information on India's efforts to combat the COVID-19 crisis.</p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Covid-19 Symptoms</h2>
+          <hr />
+<p><strong>Hight Fever 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+             <hr />
+<p><strong>Dry Cough 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+          <hr />
+<p><strong>Shortness of breath!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Prevention</h2>
+  <ul>
+            <li> <b>Vaccination:</b> Get vaccinated when eligible.</li>
+            <li> <b>Masks:</b> Masks: Wear masks in crowded or indoor settings.</li>
+            <li> <b>Hand hygiene: </b> Wash hands frequently with soap and water.</li>
+            <li> <b>Social distancing:</b> Maintain physical distance from others.</li>
+            <li> <b>Avoid crowds: </b> Limit gatherings and events.</li>
+            <li> <b>Ventilation: </b> Ensure good airflow indoors.</li>
+            <li> <b>Stay home when sick: </b> Isolate if you have symptoms.</li>
+            <li> <b>Testing:</b> Get tested if exposed or symptomatic.</li> 
+            <li> <b>Follow guidelines:</b> Follow local health authorities' advice.</li> 
+            <li> <b>Stay informed:</b>  Keep up with the latest information.</li> 
+
+
+ 
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Asit Mondal-2023</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src= "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
